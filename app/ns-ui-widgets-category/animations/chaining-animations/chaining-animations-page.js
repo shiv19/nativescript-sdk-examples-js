5fc470bf259476b3c4aa15455d656bbad82915bf
NickIliev <Nikolay.Iliev@telerik.com>
2019-07-31 13:35:21 +0300

    migrate to latest theme

--- app/ns-ui-widgets-category/animations/chaining-animations/chaining-animations-page.js
+++ app/ns-ui-widgets-category/animations/chaining-animations/chaining-animations-page.js
@@ -3,51 +3,64 @@ let myView;
 function onLoaded(args) {
     const page = args.object;
     myView = page.getViewById("lbl");
-    // >> chaining-animations
-    const duration = 300;
-    myView.animate({
-        opacity: 0,
-        duration: duration
-    }).then(() => myView.animate({
-            opacity: 1,
-            duration: duration
-        })).then(() => myView.animate({
-            translate: {
-                x: 200,
-                y: 200
-            },
-            duration: duration
-        })).then(() => myView.animate({
-            translate: {
-                x: 0,
-                y: 0
-            },
-            duration: duration
-        })).then(() => myView.animate({
-            scale: {
-                x: 5,
-                y: 5
-            },
-            duration: duration
-        })).then(() => myView.animate({
-            scale: {
-                x: 1,
-                y: 1
-            },
-            duration: duration
-        }))
-        .then(() => myView.animate({
-            rotate: 180,
-            duration: duration
-        }))
-        .then(() => myView.animate({
-            rotate: 0,
-            duration: duration
-        })).then(() => {
-            console.log("Animation finished");
-        }).catch((e) => {
-            console.log(e.message);
-        });
-    // << chaining-animations
+
+    animateView(myView);
 }
 exports.onLoaded = onLoaded;
+
+function onTap(args) {
+    const page = args.object.page;
+    myView = page.getViewById("lbl");
+
+    animateView(myView);
+}
+exports.onTap = onTap;
+
+function animateView(myView) {
+        // >> chaining-animations
+        const duration = 300;
+        myView.animate({
+            opacity: 0,
+            duration: duration
+        }).then(() => myView.animate({
+                opacity: 1,
+                duration: duration
+            })).then(() => myView.animate({
+                translate: {
+                    x: 200,
+                    y: 200
+                },
+                duration: duration
+            })).then(() => myView.animate({
+                translate: {
+                    x: 0,
+                    y: 0
+                },
+                duration: duration
+            })).then(() => myView.animate({
+                scale: {
+                    x: 5,
+                    y: 5
+                },
+                duration: duration
+            })).then(() => myView.animate({
+                scale: {
+                    x: 1,
+                    y: 1
+                },
+                duration: duration
+            }))
+            .then(() => myView.animate({
+                rotate: 180,
+                duration: duration
+            }))
+            .then(() => myView.animate({
+                rotate: 0,
+                duration: duration
+            })).then(() => {
+                console.log("Animation finished");
+            }).catch((e) => {
+                console.log(e.message);
+            });
+        // << chaining-animations
+}
